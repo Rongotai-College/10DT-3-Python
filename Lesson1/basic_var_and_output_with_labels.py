291eb86490cc502e5977f664fb232fc05fbb1a84
Edsol <66800975+edsensanz@users.noreply.github.com>
2022-08-18 12:18:47 +1200

    Add files via upload

---
+++ Lesson1/basic_var_and_output_with_labels.py
@@ -0,0 +1,9 @@
+name = "Elephant"
+numLegs = 4
+height = 4.5
+hasTail = True
+
+print("The animal:",name)
+print("The number of legs:",numLegs)
+print("Height in metres:",height)
+print("It has a tail:",hasTail)
